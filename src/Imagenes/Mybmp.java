4086ddbc72b3cb2a2faff2c6622b776fc59038aa
mariaamel99 <mariam.elamrani17@gmail.com>
2023-11-28 10:04:25 +0100

    ImagenFinal

--- src/Imagenes/Mybmp.java
+++ src/Imagenes/Mybmp.java
@@ -1,9 +1,14 @@
 package Imagenes;
 import java.io.File;
+import java.util.Scanner;
+
 public class Mybmp {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         File archivoBMP = new File("./recursos/penyagolosa.bmp");
         TransformaImagen transformador = new TransformaImagen(archivoBMP);
-        transformador.procesarImagen();
+        transformador.transformaNegativo();
+        transformador.transformaOscuro();
+        transformador.transformaBlancoNegro();
     }
 }
--- src/Imagenes/TransformaImagen.java
+++ src/Imagenes/TransformaImagen.java
@@ -5,23 +5,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 public class TransformaImagen {
     File f;
+    byte[] cabecera = new byte[54];
     public TransformaImagen(File fEnt) {
         // Control de existencia del fichero y control de la extensión .bmp (sacar
         // mensajes de error)
         if (fEnt.exists() && fEnt.isFile() && fEnt.getName().toLowerCase().endsWith(".bmp")) {
             this.f = fEnt;
-        }
-        else {
+        } else {
             System.out.println("Error: Archivo no válido o extensión incorrecta.");
         }
 
     }
+
     public void procesarImagen() {
         if (f != null) {
             try (FileInputStream file = new FileInputStream(f)) {
-                FileOutputStream file2 = new FileOutputStream("./recursos/penyagolosa_n.bmp");
                 // Leer y procesar los primeros 54 bytes para obtener información diversa
-
 
                 System.out.println("Imagen procesada exitosamente.");
             } catch (IOException e) {
@@ -31,83 +30,90 @@ public class TransformaImagen {
             System.out.println("Error: El archivo no ha sido inicializado correctamente.");
         }
     }
-    // Resto del código para procesar los bytes de la imagen (R, G, B)
-    // ...
-    public void transformaNegativo(){
+
+    public void transformaNegativo() {
         // Transformar a negativo y guardar como *_n.bmp
-        try (FileInputStream file = new FileInputStream(f)){
-            FileOutputStream file2 = new FileOutputStream("./recursos/"+getNombreSinExtension("_n")+".bmp");
-            byte[] cabecera = new byte[54];
+        try (FileInputStream file = new FileInputStream(f)) {
+            FileOutputStream file2 = new FileOutputStream("./recursos/" + getNombreSinExtension("_n") + ".bmp");
             file.read(cabecera);
             file2.write(cabecera);
             int lectura = file.read();
-            while (lectura!=-1){
-                file2.write(255- lectura);
+            while (lectura != -1) {
+                file2.write(255 - lectura);
                 lectura = file.read();
 
             }
             file.close();
             file2.close();
 
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error al procesar la imagen: " + e.getMessage());
         }
     }
-    public void transformaOscuro()  {
+
+    public void transformaOscuro() {
         // Transformar a una imagen más oscura y guardar como *_o.bmp
-        try (FileInputStream file = new FileInputStream(f)){
-            FileOutputStream file3 = new FileOutputStream("./recursos/"+getNombreSinExtension("_0")+".bmp");
-            byte[] cabecera = new byte[54];
+        try (FileInputStream file = new FileInputStream(f)) {
+            FileOutputStream file3 = new FileOutputStream("./recursos/" + getNombreSinExtension("_o") + ".bmp");
             file.read(cabecera);
             file3.write(cabecera);
             int lectura = file.read();
-            while (lectura!=-1){
-                file3.write( lectura/2);
+            while (lectura != -1) {
+                file3.write(lectura / 2);
                 lectura = file.read();
 
             }
             file.close();
             file3.close();
 
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error al procesar la imagen: " + e.getMessage());
         }
     }
 
 
-    public void transformaBlancoNegro() throws IOException {
+    public void transformaBlancoNegro() {
         // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
-        try (FileInputStream file = new FileInputStream(f)){
-            FileOutputStream file4 = new FileOutputStream("./recursos/"+getNombreSinExtension("_0")+".bmp");
-            byte[] cabecera = new byte[54];
-            byte[] pixel = new byte[3];
-            int colorNuv;
+        try (FileInputStream file = new FileInputStream(f)) {
+            FileOutputStream file4 = new FileOutputStream("./recursos/" + getNombreSinExtension("_nb") + ".bmp");
+            int R = 0, G = 0, B = 0,RGB=0;
             file.read(cabecera);
             file4.write(cabecera);
             int lectura = file.read();
-            while (lectura!=-1){
-                for(int i=0; i<3;i++){
-
-                }
-                file4.write( lectura/2);
-                lectura = file.read();
-
+           while (lectura != -1) {
+               R= lectura;
+               G= file.read();
+               B= file.read();
+               //sumar los que  son menores que 0 con 256.
+               if(R<0){
+                   R+=256;
+               }
+               if(G<0){
+                   G+=256;
+               }
+               if(B<0){
+                   B+=256;
+               }
+               int gris = (R+G+B)/3;
+               file4.write(gris);
+               file4.write(gris);
+               file4.write(gris);
+               lectura= file.read();
             }
-            file.close();
-            file4.close();
 
-        }
-        catch (IOException e){
-            System.out.println("Error al procesar la imagen: " + e.getMessage());
-        }
+        file.close();
+        file4.close();
+         }
+        catch(IOException e) {
+        System.out.println("Error al procesar la imagen: " + e.getMessage());
     }
+
+}
     private String getNombreSinExtension(String n) {
         //Devuelve el nombre del archivo f sin extensión
         String nombreImagen = f.getName();
         nombreImagen = nombreImagen.substring(0,nombreImagen.indexOf('.'));
-        return "nombreImagen"+n;
+        return nombreImagen+n;
     }
 
 
