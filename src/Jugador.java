b66bd5aa62344324727d0872035a8d39823dccaf
mariam <marela11@alu.edu.gva.es>
2023-12-05 20:53:59 +0100

    Jugadores

---
+++ src/Jugador.java
@@ -0,0 +1,19 @@
+public class Jugador {
+     public int codPais;
+     public String nombrePais;
+     public String nombreJugador;
+     public int anoNacimiento;
+     public float altura;
+     public String clubProcedencia;
+
+     public Jugador(int codPais,String nombrePais,String nombreJugador,int anoNacimiento,float altura,String clubProcedencia){
+          this.codPais = codPais;
+          this.nombrePais= nombrePais;
+          this.nombreJugador= nombreJugador;
+          this.anoNacimiento= anoNacimiento;
+          this.altura= altura;
+          this.clubProcedencia= clubProcedencia;
+     }
+
+
+}
